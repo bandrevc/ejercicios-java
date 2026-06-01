034df7d59a7f6062fdc85fbd268cd4052bf659c2
boris <bandre.vc@gmail.com>
2026-06-01 14:24:22 -0500

    feat: exercices

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main
+{
+    public static void main(String[] args) {
+        exercise1("Algoritmo de  la felicidad");
+        exercice2(5, 10);
+    }
+    /*
+    Realizar una función que te devuelva la cantidad de vocales de un texto pasado como argumento.
+    Por ejemplo:
+
+    Texto: “Algoritmo de la felicidad”
+    Cantidad de vocales: 10
+    */
+    public static void exercise1(String text) {
+        text = text.toUpperCase();
+        String[] characters = text.split("");
+        int contador = 0;
+        for (int i = 0; i < characters.length; i++) {
+            if(characters[i].equals("A") || characters[i].equals("E") ||
+                      characters[i].equals("I") || characters[i].equals("O") ||
+                      characters[i].equals("U")) {
+                contador = contador + 1;
+            }
+        }
+              System.out.println("Texto: " + text);
+              System.out.println("Cantidad de vocales: " + contador);
+    }
+    /*
+    Crear un algoritmo que multiplique 2 números sin usar el operador de multiplicación. Por ejemplo:
+    Multiplicando: 5
+    Multiplicador: 10
+    */
+    public static void exercice2(int multiplicador, int multiplicando) {
+        int contador = 0;
+        for(int i = 0; i < multiplicador; i++){
+            contador = contador + multiplicando;
+        }
+        System.out.println("Producto: " + Integer.toString(contador));
+    }
+
+}
