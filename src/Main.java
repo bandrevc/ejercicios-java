b2f3c41f9c7826f9333d45f42591f86342ec5869
boris <bandre.vc@gmail.com>
2026-06-08 22:57:30 -0500

    implementar Scanner para entrada de datos

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,19 @@
 import java.util.Arrays;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Investiga cómo leer del teclado y aplicarlo a todos los ejercicios.
         // Agregar validaciones a todos los ejercicios
-        exercise8("   Arroz con leche  ");
-        /*
-        exercise1("Algoritmo de  la felicidad");
-        exercice2(5, 10);
-        exercise3(3, 4);
-        exercise4(3, 5, 6, 2);
-        exercise5("Per aspera ad astra");
-        exercise6(4);
-        exercise7(10);
-        exercise8("Arroz con leche");
-        exercise9("Amor", "Roma");*/
+        exercise1();
+        exercise2();
+        exercise3();
+        exercise4();
+        exercise5();
+        exercise6();
+        exercise7();
+        exercise8();
+        exercise9();
     }
-
     //------------------------------------------------------------------------------------------------------------------
     /*
     Realizar una función que te devuelva la cantidad de vocales de un texto pasado como argumento.
@@ -25,15 +22,17 @@ public class Main {
     Texto: “Algoritmo de la felicidad”
     Cantidad de vocales: 10
     */
-    public static void exercise1(String text) {
-        text = text.trim().toUpperCase();
+    public static void exercise1() {
+        System.out.println("Debes ingresar un texto");
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
         if (text.isEmpty()) {
             System.out.println("Debes ingresar un texto");
         }
         else {
             String[] characters = text.split("");
             int contador = 0;
-            final String VOWELS = "AEIOU";
+            final String VOWELS = "AEIOUaeiou";
             // [""]
             for (String character : characters) {
                 if (VOWELS.contains(character))
@@ -50,15 +49,18 @@ public class Main {
     Multiplicando: 5
     Multiplicador: 10
     */
-    public static void exercice2(int multiplicador, int multiplicando) {
-        int contador = 0;
-        for (int i = 0; i < Math.abs(multiplicador); i++) {
-            contador += multiplicando;
+    public static void exercise2() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Multiplicando: ");
+        int multiplicando = sc.nextInt();
+        System.out.print("Multiplicador: ");
+        int multiplicador = sc.nextInt();
+        int producto = 0;
+        for (int i = 0; i < multiplicador; i++) {
+            producto += multiplicando;
         }
-        System.out.println("Producto: " + contador);
-
+        System.out.println("Producto: " + producto);
     }
-
     //------------------------------------------------------------------------------------------------------------------
     /*Crear una función que, dado un vector (x, y), devuelva
     su magnitud (RAIZ CUADRADA DE X^2 + Y^2). Por ejemplo:
@@ -66,14 +68,17 @@ public class Main {
     Vector componente X: 3
     Vector componente Y: 4
 */
-    public static void exercise3(int vectorX, int vectorY) {
+    public static void exercise3() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Vector componente X: ");
+        int vectorX = sc.nextInt();
+        System.out.print("Vector componente Y: ");
+        int vectorY = sc.nextInt();
         double magnitud = Math.sqrt((vectorX * vectorX) + (vectorY * vectorY));
         System.out.println("Vector componente X: " + vectorX);
         System.out.println("Vector componente Y: " + vectorY);
         System.out.println("El resultado es " + magnitud);
-
     }
-
     //------------------------------------------------------------------------------------------------------------------
     /*Crear un algoritmo que, dados dos vectores (x1, y2) y (x2, y2),
     determine su producto punto (|A|.|B| = (Ax1 * Ax2, Ay1 * Ay2). Por ejemplo:
@@ -85,19 +90,30 @@ public class Main {
 
     Producto punto: (18, 10)
 */
-    public static void exercise4(int vectorX1, int vectorY1, int vectorX2, int vectorY2) {
-        int componentX = (vectorX1 * vectorX2);
-        int componentY = (vectorY1 * vectorY2);
+    public static void exercise4() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Vector componente X1: ");
+        int vectorX1 = sc.nextInt();
+        System.out.print("Vector componente Y1: ");
+        int vectorY1 = sc.nextInt();
+        System.out.print("Vector componente X2: ");
+        int vectorX2 = sc.nextInt();
+        System.out.print("Vector componente Y2: ");
+        int vectorY2 = sc.nextInt();
+        int componentX = vectorX1 * vectorX2;
+        int componentY = vectorY1 * vectorY2;
         System.out.println("Producto punto es: (" + componentX + ", " + componentY + ")");
     }
-
     //------------------------------------------------------------------------------------------------------------------
     /*Crear un algoritmo que, dado un texto, muestre aquel texto sin vocales. Por ejemplo:
 
         Texto: “Per aspera ad astra”
         Texto sin vocales: “Pr spr d str”
 */
-    public static void exercise5(String text) {
+    public static void exercise5() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escribe un texto: ");
+        String text = sc.nextLine();
         String[] characters = text.split("");
         // "manuel" -> ["m", "a", ...] -> ["m", "", "n", "", "", "l"]
         final String VOWELS = "aeiou";
@@ -121,7 +137,10 @@ public class Main {
          4 x 4 = 16
          ...
    */
-    public static void exercise6(int multiplier) {
+    public static void exercise6() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escriba un numero: ");
+        int multiplier = sc.nextInt();
         System.out.println("Número:" + multiplier);
         System.out.println("Tabla de multiplicar:");
         for (int multiplying = 1; multiplying <= 12; multiplying++) {
@@ -136,7 +155,10 @@ public class Main {
             Número n: 10
             Suma de 1 a n: 1 + 2 + 3… + 10 = 55
     */
-    public static void exercise7(int lastTerm) {
+    public static void exercise7() {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Escriba un numero: ");
+        int lastTerm = sc.nextInt();
         System.out.println("Numero n: " + lastTerm);
         int sum = calculateNaturalNumberSum(lastTerm);
         String numberChain = "";
@@ -155,7 +177,10 @@ public class Main {
         Texto: “Arroz con leche”
         Texto al revés: “ehcel noc zorrA”
 */
-    public static void exercise8(String text) {
+    public static void exercise8() {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Escriba un texto: ");
+        String text = sc.nextLine();
         text = text.trim();
         System.out.println("Texto: " + text);
         String[] characters = text.split("");
@@ -182,13 +207,14 @@ public class Main {
         Segunda palabra: “roma”
         Es un anagrama
     */
-    public static void exercise9(String first, String second) {
-        System.out.println("Primera palabra: " + first);
-        System.out.println("Segunda palabra: " + second);
-        first = first.toUpperCase();
-        second = second.toUpperCase();
-        String[] charactersFirst = first.split("");
-        String[] charactersSecond = second.split("");
+    public static void exercise9() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Primera palabra: ");
+        String first = sc.nextLine();
+        System.out.print("Segunda palabra: ");
+        String second = sc.nextLine();
+        char[] charactersFirst = first.toUpperCase().toCharArray();
+        char[] charactersSecond = second.toUpperCase().toCharArray();
         Arrays.sort(charactersFirst);
         Arrays.sort(charactersSecond);
         if (Arrays.equals(charactersFirst, charactersSecond)) {
